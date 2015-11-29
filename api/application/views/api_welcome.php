236fb025cbca1472df50e3304faa124286814df4
alfedalinan <alfed.alinan@gmail.com>
2015-11-29 23:53:58 +0800

    initial commit - api

---
+++ api/application/views/api_welcome.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Pack Institute API - Index
+	</title>
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>">
+
+</head>
+<body>
+
+<!-- NAVBAR: END -->
+
+<nav id="pack-nav" class="navbar navbar-default" role="navigation">
+	<div class="navbar-header">
+      <a class="navbar-brand" href="<?php echo base_url(); ?>"> Pack Institute API </a>
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-target">
+	    <span class="icon-bar"></span>
+	    <span class="icon-bar"></span>
+	    <span class="icon-bar"></span>
+	  </button>
+   </div>
+   
+  <div id="navbar-target" class="navbar-collapse collapse" aria-expanded="true">
+		<ul class="nav navbar-nav navbar-right">
+			<li><a href="#"> Enrollment System </a></li>
+			<li><a href="#"> Medical System </a></li>
+			<li><a href="#"> Mobile Development </a></li>
+		</ul>
+	</div>
+</nav>
+
+<!-- BODY -->
+<div id="wrap" class="container">
+	<div id="title" class="jumbotron text-center">
+		<h2> Welcome to Pack Institute API </h2>
+	</div>
+	<div class="row">
+		<div class="col-sm-4">
+	      <h4> <u> Enrollment System </u> </h4>
+	      <p> Computerized, electronic, and robust system in school's or university's enrollment system. </p>
+	      <a href="#"> Learn more </a>
+	    </div>
+	    <div class="col-sm-4">
+	      <h4> <u> Medical System </u></h4>
+	      <p> More manageable patient record handling, as easy as typing and tapping on your device. </p>
+	      <a href="#"> Learn more </a>
+	    </div>
+	    <div class="col-sm-4">
+	      <h4> <u> Mobile Development System </u> </h4>
+	      <p> More technology trend developments are to come with our continuous studies and researches. </p>
+	      <a href="#"> Learn more </a>
+	    </div>
+	  </div>
+	</div>
+</div>
+
+<!-- FOOTER -->
+
+<div id="stick"></div>
+<footer id="footer" class="footer">
+	<div class="container">
+		<div class="right">By: Gemini St. Development Team </div>
+	</div>
+</footer>
+
+</body>
+<script src="<?php echo base_url('assets/js/jquery-1.11.3.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+
+</html>
